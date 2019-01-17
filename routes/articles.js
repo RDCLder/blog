a58aef9dcddb89d584d396afe895b8229a786529
RDCLder <riy2@case.edu>
2019-01-17 13:54:01 -0600

    test

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models/database.js");
+const body_parser = require('body-parser');
 
 router.get("/article/:articleID", (req, res) => {
     
@@ -14,6 +15,23 @@ router.get("/article/:articleID", (req, res) => {
             })
         })
 
+
+    });
+    router.use(body_parser.urlencoded({ extended: false }));
+    router.post("/article/:articleID", (req, res) => {
+        var username = req.body.username;
+        var body = req.body.body;
+
+        db.none("INSERT INTO comments(username, body, blog_id )VALUES($1,$2,$3)", [username, body, articleID])
+            .then((data) => {
+                console.log(articleID);
+                // db.any('SELECT * FROM')
+                console.log("Success");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
     // db.query("SELECT * FROM articles")
     // .then((results) => {
     //     res.render("article", {
@@ -24,5 +42,6 @@ router.get("/article/:articleID", (req, res) => {
     // });
 
 });
+
 
 module.exports = router;
